e8ddc68732d70d162f38db56ac15dbf05b588d55
omjraut07 <91650885+omjraut07@users.noreply.github.com>
2021-09-30 12:12:07 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,13 +1,7 @@
-module.exports = {
-  ...require('./auth'),
-  ...require('./poll'),
-};
+const mongoose = require('mongoose');
+mongoose.set('debug', true);
+mongoose.Promise = global.Promise;
+mongoose.connect(process.env.DATABASE);
 
-module.exports.error = (err, req, res, next) => {
-  return res.status(err.status || 500).json({
-    success: false,
-    error: {
-      message: err.message || 'Something went wrong.',
-    },
-  });
-};
+module.exports.User = require('./user');
+module.exports.Poll = require('./poll');
