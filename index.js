7275ded6e065d4f68f25a476e2ad7f20b8aac3a4
omjraut07 <91650885+omjraut07@users.noreply.github.com>
2021-09-30 12:05:53 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,9 +1,3 @@
-import React from 'react';
-import { render } from 'react-dom';
-
-import './index.css';
-import App from './containers/App';
-import registerServiceWorker from './registerServiceWorker';
-
-render(<App />, document.getElementById('root'));
-registerServiceWorker();
+export * from './auth';
+export * from './error';
+export * from './polls';
